e9cf3ce6d96c70b8e9c3a16269d16a51ea71a90b
TuDouZai <15879772+tudouza1@user.noreply.gitee.com>
2026-02-22 17:23:14 +0800

    fix(build): 修改基础路径以匹配新项目名称
    - 将vite配置文件中的base路径从'/TuDouLog/'修改为'/tudoulog-vue/'
    - 保持插件配置不变，确保Vue处理.md文件功能正常
    - 通过调整路径解决部署或访问时的静态资源加载问题

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import Markdown from 'vite-plugin-vue-markdown'
 import prism from 'markdown-it-prism' // 改用 import 导入
 
 export default defineConfig({
-  base: '/TuDouLog/',
+  base: '/tudoulog-vue/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/] // 让 Vue 处理 .md 文件
